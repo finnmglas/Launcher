20f2c6ccabc69cbf3db3d86cf7d01d4e44749a20
Finn M Glas <finnmglas@gmail.com>
2020-05-15 07:51:48 +0200

    Add code for a settings activity

---
+++ app/src/main/java/com/finnmglas/launcher/SettingsActivity.kt
@@ -0,0 +1,59 @@
+package com.finnmglas.launcher
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.ResolveInfo
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.KeyEvent
+import android.view.MotionEvent
+import android.view.View
+import android.view.WindowManager
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MotionEventCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.abs
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.concurrent.fixedRateTimer
+
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == 5000)
+        {
+            val value = data?.getStringExtra("value")
+            val forApp = data?.getStringExtra("forApp") ?: return
+
+            if (forApp == "downApp") downApp = value.toString()
+            else if (forApp == "upApp") upApp = value.toString()
+            else if (forApp == "leftApp") leftApp = value.toString()
+            else if (forApp == "rightApp") rightApp = value.toString()
+            else if (forApp == "volumeDownApp") volumeDownApp = value.toString()
+            else if (forApp == "volumeUpApp") volumeUpApp = value.toString()
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    fun chooseApp() {
+        val intent = Intent(this, ChooseActivity::class.java)
+        intent.putExtra("action", "pick") // why choose an app
+        intent.putExtra("forApp", "downApp") // which app we choose
+        startActivityForResult(intent, 5000)
+    }
+
+    @SuppressLint("SetTextI18n") // I do not care
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        setContentView(R.layout.activity_settings)
+    }
+}
