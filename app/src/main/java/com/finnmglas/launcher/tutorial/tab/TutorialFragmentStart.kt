ddf30ece3edf76f6348b4aa007069ee404aa1dd7
Finn M Glas <finn@finnmglas.com>
2020-06-23 17:43:03 +0200

    Fill the `Start` tab (1) of the tutorial
    Use blinking icons to indicate the swipe direction. Text in the middle 
    still is the same. Remove page title.

--- app/src/main/java/com/finnmglas/launcher/tutorial/tab/TutorialFragmentStart.kt
+++ app/src/main/java/com/finnmglas/launcher/tutorial/tab/TutorialFragmentStart.kt
@@ -29,5 +29,16 @@ class TutorialFragmentStart(): Fragment(), UIObject {
 
     override fun applyTheme() {
         tutorial_start_container.setBackgroundColor(dominantColor)
+
+        // set icons
+        val rightIcon = getString(R.string.fas_angle_double_right)
+
+        tutorial_start_icon_right.text = rightIcon.repeat(3)
+        tutorial_start_icon_right.setTextColor(vibrantColor)
+        tutorial_start_icon_right.blink()
+
+        tutorial_start_icon_right_2.text = rightIcon.repeat(3)
+        tutorial_start_icon_right_2.setTextColor(vibrantColor)
+        tutorial_start_icon_right_2.blink()
     }
 }
